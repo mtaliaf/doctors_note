7075d74ca3dba22bff688e906087b4a5e1aa9536
mtaliaf <marquis.taliaferro@gmail.com>
2016-01-30 23:32:41 -0600

    Initial Commit

---
+++ src/main/java/com/doctors_note/servlets/DoctorsNoteServletConfig.java
@@ -0,0 +1,19 @@
+package com.doctors_note.servlets;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+import com.google.inject.servlet.ServletModule;
+
+public class DoctorsNoteServletConfig extends GuiceServletContextListener {
+
+  @Override
+  protected Injector getInjector() {
+    return Guice.createInjector(new ServletModule() {
+      @Override
+      protected void configureServlets() {
+        serve("/test").with(TestServlet.class);
+      }
+    });
+  }
+}
---
+++ src/test/java/com/doctors_note/servlets/PersonTest.java
@@ -0,0 +1,8 @@
+package com.doctors_note.servlets;
+
+import org.junit.Test;
+
+public class PersonTest {
+  @Test
+  public void canReturnResponse() {}
+}
